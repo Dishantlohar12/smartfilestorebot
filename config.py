a6aae1be62daac5c5f15c2fd747ec7852973c707
Dishantlohar12 <malviyadishant729@gmail.com>
2026-02-16 19:59:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
-API_ID = int(os.getenv("API_ID", "267324"))
+API_ID = int(os.getenv("API_ID", "26590590"))
 API_HASH = os.getenv("API_HASH", "4805ee1d57b1be7f5135e736c816a2d1")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8563648313:AAEvDqOEyhk3T3TvlTpJLxeGDDGw08_GjTY")
 
-DATABASE_URL = os.getenv("DATABASE_URL", "mongodb://localhost:27017")
+DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://nshubh345:1FmseyW0TKaWNMNo@cluster0.pgewb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-ADMINS = [int(x) for x in os.getenv("ADMINS", "").split() if x]
+ADMINS = [int(x) for x in os.getenv("ADMINS", "5904478052").split() if x]
