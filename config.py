97b0cd4163966e1b6e35f7ea665b78fc83ac3238
Dishantlohar12 <malviyadishant729@gmail.com>
2026-02-16 19:58:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-API_ID = int(os.getenv("API_ID", "123456"))
-API_HASH = os.getenv("API_HASH", "your_api_hash")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
+API_ID = int(os.getenv("API_ID", "267324"))
+API_HASH = os.getenv("API_HASH", "4805ee1d57b1be7f5135e736c816a2d1")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8563648313:AAEvDqOEyhk3T3TvlTpJLxeGDDGw08_GjTY")
 
 DATABASE_URL = os.getenv("DATABASE_URL", "mongodb://localhost:27017")
 
